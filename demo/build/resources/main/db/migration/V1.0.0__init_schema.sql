bd395a02d7c39b1f38835b22fd19fb9b22a1a3d5
nguyenbanh1 <35592951+nguyenbanh1@users.noreply.github.com>
2022-08-10 01:40:53 +0700

    Delete demo directory

--- demo/build/resources/main/db/migration/V1.0.0__init_schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `employee` (
-    `id`                INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name`              VARCHAR(250) NOT NULL UNIQUE,
-    `supervisor`        INTEGER,
-    `version`           INTEGER NOT NULL,
-    `created_date`      TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP(3),
-    `modified_date`     TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP(3),
-    FOREIGN KEY(supervisor) REFERENCES employee(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE = utf8mb4_unicode_ci;
